86126061c45332b40905f029199bb4b47d0f2275
MrLotU <j.koopman@jarict.nl>
2017-07-19 09:39:37 +0200

    Initial Commit

---
+++ Marvelous/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marvelous
+//
+//  Created by Jari Koopman on 19/07/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Marvelous/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marvelous
+//
+//  Created by Jari Koopman on 19/07/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
