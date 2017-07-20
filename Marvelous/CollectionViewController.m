f490898a4abe803fecf2825fe37938c92c084c48
MrLotU <j.koopman@jarict.nl>
2017-07-20 15:36:00 +0200

    Adde some more files

--- Marvelous/CollectionViewController.m
+++ Marvelous/CollectionViewController.m
@@ -88,15 +88,16 @@ static NSString * const reuseIdentifier = @"characterCell";
 }
 
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
 }
-*/
+
 
 #pragma mark <UICollectionViewDataSource>
 
---
+++ Marvelous/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Marvelous
+//
+//  Created by Jari Koopman on 20/07/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
